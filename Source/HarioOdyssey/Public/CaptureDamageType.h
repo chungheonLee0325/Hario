9fef39387b72b1978aa72a3ca21ba1bbc6be9702
ChungChungLee <chungheon.lee0325@gmail.com>
2025-01-05 20:53:41 +0900

    Common:폴더정리 Cpp:코드 sample 추가

---
+++ Source/HarioOdyssey/Public/CaptureDamageType.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "CaptureDamageType.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HARIOODYSSEY_API UCaptureDamageType : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/HarioOdyssey/Public/Hario_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Hario_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HARIOODYSSEY_API UHario_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

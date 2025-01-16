66e08c6a5de33a0e356a34fad45aa980b0d2d6f3
zigzaghyun <168054741+zigzaghyun@users.noreply.github.com>
2025-01-16 17:42:44 +0900

    UI : 생성 주체 Controller로 변경 -> Player_Mario는 여러개 생기거나 변경될수 있으므로(캡쳐) 한개뿐인 Controller로 생성 위임 / Player 는 포인터 객체만 참조 Hat : 입력맵핑 수정 및 던져서 돌아오기 구현 완료

---
+++ Source/HarioOdyssey/AreaObject/Player/MarioController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MarioController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HARIOODYSSEY_API AMarioController : public APlayerController
+{
+	GENERATED_BODY()
+};

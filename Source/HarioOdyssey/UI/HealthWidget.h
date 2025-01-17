5acc691924aa6e63b00cedd396568941bb744b3b
zigzaghyun <168054741+zigzaghyun@users.noreply.github.com>
2025-01-17 11:22:25 +0900

    Condition : 버그수정(LCH) Health Widget :  UI 구현 Player Start : 카메라 거리 수정

---
+++ Source/HarioOdyssey/UI/HealthWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HARIOODYSSEY_API UHealthWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
